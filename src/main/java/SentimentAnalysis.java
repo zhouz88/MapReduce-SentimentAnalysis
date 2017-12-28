3ead139dd6611348ce2fe42e57947ac4bb3bc11e
Kerwin <zhouz88@uw.edu>
2017-12-28 00:26:00 -0800

    Update SentimentAnalysis.java

--- src/main/java/SentimentAnalysis.java
+++ src/main/java/SentimentAnalysis.java
@@ -3,6 +3,7 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
