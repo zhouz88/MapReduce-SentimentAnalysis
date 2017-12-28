cf9072bf647a5cb80330e82acdd85bfdfcd86beb
Kerwin <zhouz88@uw.edu>
2017-12-28 00:27:51 -0800

    Update JSONConverter.java

--- src/main/java/JSONConverter.java
+++ src/main/java/JSONConverter.java
@@ -5,6 +5,7 @@ import org.codehaus.jettison.json.JSONObject;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
